8620126651cd24e11622af0f6c2d39355fa165b0
tizee <33030965+tizee@users.noreply.github.com>
2020-10-18 23:35:44 +0800

    feat: use control-socket for tunnling

--- ssh_tunneling.sh
+++ ssh_tunneling.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-ssh -fnNTL 9999:github.com:22 username@server_ip
+# $1 socket-file $2 username@server_ip
+ssh -M -S $1 -fnNTL 9999:github.com:22 $2
+# check running
+# ssh -S socket-file -O check username@server_ip
+# exit
+# ssh -S socket-file -O exit username@server_ip
